414d5b9dea651112a25a350a33da76ba0744d340
matthewogtong <ogtongm@gmail.com>
2021-03-03 00:34:50 -0500

    fixed tag issues, removed tagSlice and changed to user entities, added tag actions to userSlice

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,14 +2,12 @@ import { configureStore } from "@reduxjs/toolkit"
 import userReducer from "./user"
 import regionReducer from "./region"
 import trinketReducer from "./trinket"
-import tagReducer from "./tag"
 
 const store = configureStore({
     reducer: {
         user: userReducer,
         region: regionReducer,
-        trinket: trinketReducer,
-        tag: tagReducer
+        trinket: trinketReducer
     }
 })
 
