8e5828b7f3543ede807d39537a30c3f7fbc4955e
matthewogtong <ogtongm@gmail.com>
2021-03-03 20:58:22 -0500

    flexboxed InTheZone component and adjusted textContent for form

--- src/home/middle/InTheZone.js
+++ src/home/middle/InTheZone.js
@@ -1,9 +1,28 @@
 import React from 'react'
+import { useHistory } from "react-router-dom"
+import { useSelector } from "react-redux"
 
 const InTheZone = () => {
+    const history = useHistory()
+
+    const currentZone = useSelector(state => state.user.entities[0].zones.slice(-1)[0])
+    
+    const handleClick = () => {
+        history.push("/home")
+    }
+
     return (
-        <h1>in the zone</h1>
-    )
+      <div className="in-zone-div">
+        <h1>Currently in Zone</h1>
+        <h3>Objective: {currentZone.objective}</h3>
+        <h3>Total Expected Zone Time: {currentZone.totalObjectiveTime} minutes</h3>
+        <h3>Start Time: {currentZone.zoneStart}</h3>
+        <h3>Time Remaining: </h3>
+        <h3>Region: {currentZone.region.name}</h3>
+        <h3>Trinket being crafted</h3>
+        <button onClick={e => handleClick()}>End Zone</button>
+      </div>
+    );
 }
 
 export default InTheZone
