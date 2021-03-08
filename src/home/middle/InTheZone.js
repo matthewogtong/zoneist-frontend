6f146c31111a992e269efccca77ff9359c5b2d6f
matthewogtong <ogtongm@gmail.com>
2021-03-08 10:32:23 -0500

    implement react-countdown hook to display timer

--- src/home/middle/InTheZone.js
+++ src/home/middle/InTheZone.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useHistory } from "react-router-dom"
 import { useSelector } from "react-redux"
+import Countdown from 'react-countdown'
 
 const InTheZone = () => {
     const history = useHistory()
@@ -10,7 +11,23 @@ const InTheZone = () => {
     const handleClick = () => {
         history.push("/home")
     }
+
+    const Completionist = () => {
+      return (
+        <span>you completed your zone!</span>
+      )
+    }
     
+    const renderer = ({ hours, minutes, seconds, completed }) => {
+      if (completed) {
+        return <Completionist />
+      } else {
+        return <span>{hours}:{minutes}:{seconds}</span>
+      }
+    }
+
+    const totalCountdownMilliseconds = currentZone.totalObjectiveTime * 1000
+    console.log(totalCountdownMilliseconds)
 
     return (
       <div className="in-zone-div">
@@ -18,6 +35,10 @@ const InTheZone = () => {
         <h3>Objective: {currentZone.objective}</h3>
         <h3>Total Expected Zone Time: {currentZone.totalObjectiveTime} minutes</h3>
         <h3>Start Time: {currentZone.zoneStart}</h3>
+        <Countdown 
+          date={Date.now() + totalCountdownMilliseconds}
+          renderer={renderer}
+        />
         <h3>Time Remaining: </h3>
         <h3>Region: {currentZone.region.name}</h3>
         <h3>Trinket being crafted: {currentZone.trinket.name}</h3>
