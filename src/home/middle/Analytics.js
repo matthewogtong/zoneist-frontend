a48cd2251fa09176d1b3c6ecfce5a5ee74266c8b
matthewogtong <ogtongm@gmail.com>
2021-03-27 18:57:24 -0400

    adjusted weekly date for barChartData - Analytics Component

--- src/home/middle/Analytics.js
+++ src/home/middle/Analytics.js
@@ -48,7 +48,7 @@ const Analytics = () => {
         labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
         datasets: [
             {
-                label: 'week of 3/29/2021',
+                label: 'week of 4/5/2021',
                 backgroundColor: '#ffb199',
                 data: [630, 600, 660, 260, 0, 0, 0]
             }
